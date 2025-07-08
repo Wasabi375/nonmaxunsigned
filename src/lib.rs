15ec86957a4ad158ac370d182458847d3ef6ace4
Burkhard Mittelbach <wasabi37a@googlemail.com>
2025-07-08 22:59:01 +0200

    Integer operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,266 @@
-#![no_std]
+//! A create providing nonmax integer types
+//!
+//! TODO Example
+//!
+//! TODO compare to existing crates
 
+#![no_std]
+#![deny(missing_docs)]
+
+use core::ops::{
+    Add, AddAssign, BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Div, DivAssign,
+    Mul, MulAssign, Sub, SubAssign,
+};
+
+/// U7 is a nonmax version of [u8]
 #[derive(Clone, Copy)]
 #[repr(transparent)]
 #[allow(dead_code)]
 pub struct U7(U7Internal);
 
+/// U15 is a nonmax version of [u16]
 #[derive(Clone, Copy)]
 #[repr(C)]
 #[allow(dead_code)]
 #[cfg(target_endian = "little")]
 pub struct U15(U7, u8);
+/// U15 is a nonmax version of [u16]
 #[cfg(target_endian = "big")]
 #[derive(Clone, Copy)]
 #[repr(C)]
 #[allow(dead_code)]
 pub struct U15(u8, U7);
 
+/// U31 is a nonmax version of [u32]
 #[derive(Clone, Copy)]
 #[repr(C)]
 #[allow(dead_code)]
 #[cfg(target_endian = "little")]
 pub struct U31(U7, u8, u16);
+/// U31 is a nonmax version of [u32]
 #[derive(Clone, Copy)]
 #[repr(C)]
 #[allow(dead_code)]
 #[cfg(target_endian = "big")]
 pub struct U31(u16, u8, U7);
 
+/// U63 is a nonmax version of [u64]
 #[derive(Clone, Copy)]
 #[repr(C)]
 #[allow(dead_code)]
 #[cfg(target_endian = "little")]
 pub struct U63(U7, u8, u16, u32);
+/// U63 is a nonmax version of [u64]
 #[derive(Clone, Copy)]
 #[repr(C)]
 #[allow(dead_code)]
 #[cfg(target_endian = "big")]
 pub struct U63(u32, u16, u8, U7);
 
+// TODO: when I implement simple_endian crate I should look into whether
+// it is even valid to store U7 as a big endian on a little endian system.
+// It should also always be invalid to store U15 as big on little endian
+// as the U7 is then covered by a u8 value
+//
+// Possible fix is to use MaybeUninit but that would probably require a change
+// to the simple_endian crate
+
+// NOTE: copy past from rust-lang: https://github.com/rust-lang/rust/blob/ab68b0fb26485ab1fa6977b2d8b59cc8a171c4aa/library/core/src/internal_macros.rs
+macro_rules! forward_ref_binop {
+    (impl $imp:ident, $method:ident for $t:ty, $u:ty) => {
+        impl<'a> $imp<$u> for &'a $t {
+            type Output = <$t as $imp<$u>>::Output;
+
+            #[inline]
+            #[track_caller]
+            fn $method(self, other: $u) -> <$t as $imp<$u>>::Output {
+                $imp::$method(*self, other)
+            }
+        }
+
+        impl $imp<&$u> for $t {
+            type Output = <$t as $imp<$u>>::Output;
+
+            #[inline]
+            #[track_caller]
+            fn $method(self, other: &$u) -> <$t as $imp<$u>>::Output {
+                $imp::$method(self, *other)
+            }
+        }
+
+        impl $imp<&$u> for &$t {
+            type Output = <$t as $imp<$u>>::Output;
+
+            #[inline]
+            #[track_caller]
+            fn $method(self, other: &$u) -> <$t as $imp<$u>>::Output {
+                $imp::$method(*self, *other)
+            }
+        }
+    };
+}
+
+// NOTE: copy past from rust-lang: https://github.com/rust-lang/rust/blob/ab68b0fb26485ab1fa6977b2d8b59cc8a171c4aa/library/core/src/internal_macros.rs
+macro_rules! forward_ref_op_assign {
+    (impl $imp:ident, $method:ident for $t:ty, $u:ty) => {
+        impl $imp<&$u> for $t {
+            #[inline]
+            #[track_caller]
+            fn $method(&mut self, other: &$u) {
+                $imp::$method(self, *other);
+            }
+        }
+    };
+}
+
+macro_rules! impl_binop {
+    (impl $imp:ident, $method:ident, $type:ty) => {
+        impl $imp for $type {
+            type Output = $type;
+
+            fn $method(self, other: $type) -> $type {
+                let primitive = self.get().add(other.get());
+                match Self::new(primitive) {
+                    Some(res) => res,
+                    None => panic!(
+                        "{}::{} resulted in forbidden max value({}). Use `checked_{}` instead",
+                        stringify!($type),
+                        stringify!($method),
+                        Self::INVALID_UNDERLYING,
+                        stringify!($method),
+                    ),
+                }
+            }
+        }
+        forward_ref_binop!(impl $imp, $method for $type, $type);
+    };
+}
+
+macro_rules! impl_assign_op {
+    (impl $imp:ident, $method:ident, $binop:ident, $op:ident, $type:ty) => {
+        impl $imp for $type
+        where
+            $type: $binop,
+        {
+            fn $method(&mut self, other: $type) {
+                *self = self.$op(other)
+            }
+        }
+        forward_ref_op_assign!(impl $imp, $method for $type, $type);
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! option_op {
+    ($type:ty, $op:ident, $(doc = $doc:tt)*) => {
+        $(#[doc = $doc])*
+        pub const fn $op(self, rhs: $type) -> Option<$type> {
+            match self.get().$op(rhs.get()) {
+                Some(primitive) => <$type>::new(primitive),
+                None => None,
+            }
+        }
+    };
+}
+
+#[cfg(feature = "checked-ops")]
+macro_rules! checked_ops {
+    ($type:ty) => {
+        option_op!($type, checked_add,
+            doc = "Checked integer addition. Computes `self + rhs`, returning `None` if overflow occured."
+        );
+        option_op!($type, checked_sub,
+            doc = "Checked integer subtraction. Computes `self - rhs`, returning `None` if overflow occured."
+        );
+        option_op!($type, checked_mul,
+            doc = "Checked integer multiplication. Computes `self * rhs`, returning `None` if overflow occured."
+        );
+        option_op!($type, checked_div,
+            doc = "Checked integer division. Computes `self / rhs`, returning `None` if overflow occured."
+        );
+        option_op!($type, checked_div_euclid,
+            doc = "Checked Euclidean division. Computes `self.div_euclid(rhs)`, returning `None` if overflow occured."
+            doc = ""
+            doc = "Strict division on unsigned types is just normal division. There’s no way overflow could ever happen."
+            doc = "This function exists so that all operations are accounted for in the strict operations."
+            doc = "Since, for the positive integers, all common definitions of division are equal, this is exactly equal to self.strict_div(rhs)."
+        );
+        option_op!($type, checked_rem,
+            doc = "Checked integer division. Computes `self % rhs`, returning `None` if overflow occured."
+        );
+
+        #[doc = "Checked integer division. Computes `self << rhs`, returning `None` if overflow occured."]
+        pub const fn checked_shl(self, rhs:u32) -> Option<$type>{
+            match self.get().checked_shl(rhs){
+                Some(primitive) =>  <$type>::new(primitive),
+                None => None,
+            }
+        }
+
+        #[doc = "Checked integer division. Computes `self >> rhs`, returning `None` if overflow occured."]
+        pub const fn checked_shr(self, rhs:u32) -> Option<$type>{
+            match self.get().checked_shr(rhs){
+                Some(primitive) =>  <$type>::new(primitive),
+                None => None,
+            }
+        }
+
+        /// Returns the logarithm of the number with respect to an arbitrary base, rounded down.
+        ///
+        /// Returns `None` if the number is zero, or if the base is not at least 2.
+        ///
+        /// This method might not be optimized owing to implementation details;
+        /// `checked_ilog2` can produce results more efficiently for base 2, and `checked_ilog10` can produce results more efficiently for base 10.
+        pub const fn checked_ilog(self, base: $type) -> Option<u32> {
+            self.get().checked_ilog(base.get())
+        }
+
+        /// Returns the base 2 logarithm of the number, rounded down.
+        ///
+        /// Returns `None` if the number is zero.
+        pub const fn checked_ilog2(self) -> Option<u32> {
+            self.get().checked_ilog2()
+        }
+
+        /// Returns the base 10 logarithm of the number, rounded down.
+        ///
+        /// Returns `None` if the number is zero.
+        pub const fn checked_ilog10(self) -> Option<u32> {
+            self.get().checked_ilog10()
+        }
+    };
+}
+
 macro_rules! non_max_impl {
     ($type:ty, $primitive:ty, $test_name:ident) => {
         impl $type {
+
+            /// The smallest value that can be respresented by this integer type
             pub const MIN: Self = unsafe {
                 // Safety: 0 is never max
                 Self::new_unchecked(0)
             };
+
+            /// The largest value that can be respresented by this integer type
             pub const MAX: Self = unsafe {
                 // Safety: max - 1 is never max
                 Self::new_unchecked(<$primitive>::MAX - 1)
             };
 
+            /// The size of this integer type in bits
+            pub const BITS: u32 = <$primitive>::BITS - 1;
+
+            /// The size of the underlying interger type in bits
+            ///
+            #[doc = concat!("This is the same as `BITS` on the result type of [Self::get] ([", stringify!($primitive), "])")]
+            pub const PRIMITIVE_BITS: u32 = <$primitive>::BITS;
+
+            /// The value of the underlying integer that can *not* be represented
+            pub const INVALID_UNDERLYING: $primitive = <$primitive>::MAX;
+
+            /// Create a new [Self] or `None` if value is the max of the underlying integer type
             pub const fn new(value: $primitive) -> Option<Self> {
-                if value == <$primitive>::MAX {
+                if value == Self::INVALID_UNDERLYING {
                     None
                 } else {
                     unsafe {
@@ -61,7 +270,13 @@ macro_rules! non_max_impl {
                 }
             }
 
+            /// Create a new [Self] without checking if the value is the max of the underlying integer type
+            ///
+            /// # Safety
+            ///
+            #[doc = concat!("`value` must not be `", stringify!($primitive), "::MAX`")]
             pub const unsafe fn new_unchecked(value: $primitive) -> Self {
+                assert!(value != Self::INVALID_UNDERLYING);
                 unsafe {
                     // Safety: $primitive and Self have the same size.
                     // Crate ensures that the only invalid bitpattern for Self
@@ -70,13 +285,128 @@ macro_rules! non_max_impl {
                 }
             }
 
-            pub fn get(self) -> $primitive {
+            /// Return the underlying integer type
+            pub const fn get(self) -> $primitive {
                 unsafe {
                     // Safety: primitive type can be created from any bit-pattern
                     // and $type and $primtive have the same size
                     core::mem::transmute_copy(&self)
                 }
             }
+
+            /// Computes the absolute difference between `self` and `other`.
+            pub const fn abs_diff(self,other:Self) -> Self {
+                let primitive = self.get().abs_diff(other.get());
+                unsafe {
+                    // Safety: diff is nerver max
+                    Self::new_unchecked(primitive)
+                }
+            }
+
+            /// Calculates the quotient of self and rhs, rounding the result towards positive infinity.
+            pub const fn div_ceil(self,other:Self) -> Self {
+                let primitive = self.get().div_ceil(other.get());
+                unsafe {
+                    // Safety: div is nerver max
+                    Self::new_unchecked(primitive)
+                }
+            }
+
+            /// Returns `true` if `self` is an integer multiple of `rhs`, and false otherwise.
+            ///
+            /// This function is equivalent to self % rhs == 0, except that it will not panic for `rhs == 0`.
+            /// Instead, `0.is_multiple_of(0) == true`, and for any non-zero `n`, `n.is_multiple_of(0) == false`.
+            pub const fn is_multiple_of(self, rhs: Self) -> bool {
+                self.get().is_multiple_of(rhs.get())
+            }
+
+            /// Returns true if and only if `self == 2^k` for some unsigned integer `k`.
+            pub const fn is_power_of_two(self) -> bool {
+                self.get().is_power_of_two()
+            }
+
+            /// Returns the smallest power of two greater than or equal to self. If the next power of two is
+            /// greater than the type’s maximum value, `None` is returned, otherwise the power of two is wrapped in Some.
+            pub const fn checked_next_power_of_two(self) -> Option<Self> {
+                match self.get().checked_next_power_of_two() {
+                    Some(primitive) => Self::new(primitive),
+                    None => None
+                }
+            }
+
+            /// Calculates the smallest value greater than or equal to self that is a multiple of rhs. Returns None if rhs is zero or the operation would result in overflow.
+            pub const fn checked_next_multiple_of(self,other:Self) -> Option<Self> {
+                match self.get().checked_next_multiple_of(other.get()) {
+                    Some(primitive) => Self::new(primitive),
+                    None => None
+                }
+            }
+
+            /// Calculates the midpoint (average) between self and rhs.
+            ///
+            /// `midpoint(a, b)` is `(a + b) / 2` as if it were performed in a sufficiently-large unsigned integral type.
+            /// This implies that the result is always rounded towards zero and that no overflow will ever occur.
+            pub const fn midpoint(self,other:Self) -> Self {
+                let primitive = self.get().midpoint(other.get());
+                unsafe {
+                    // Safety: midpoint is nerver max
+                    Self::new_unchecked(primitive)
+                }
+            }
+
+            /// Returns the logarithm of the number with respect to an arbitrary base, rounded down.
+            ///
+            /// This method might not be optimized owing to implementation details;
+            /// `ilog2` can produce results more efficiently for base 2, and `ilog10` can produce results more efficiently for base 10.
+            ///
+            /// #Panics
+            ///
+            /// This function will panic if self is zero, or if base is less than 2.
+            pub const fn ilog(self, base: Self) -> u32 {
+                self.get().ilog(base.get())
+            }
+
+
+            /// Returns the base 2 logarithm of the number, rounded down.
+            ///
+            /// # Panics
+            ///
+            /// This function will panic if self is zero.
+            pub const fn ilog2(self) -> u32 {
+                self.get().ilog2()
+            }
+
+            /// Returns the base 10 logarithm of the number, rounded down.
+            ///
+            /// # Panics
+            ///
+            /// This function will panic if self is zero.
+            pub const fn ilog10(self) -> u32 {
+                self.get().ilog10()
+            }
+
+            /// Returns the square root of the number, rounded down.
+            pub const fn isqrt(self) -> Self {
+                let primitive = self.get().isqrt();
+                unsafe {
+                    // Safety: squrt is never max
+                    Self::new_unchecked(primitive)
+                }
+            }
+
+            /// Returns the number of ones in the binary representation of `self`.
+            pub const fn count_ones(self) -> u32 {
+                self.get().count_ones()
+            }
+
+            /// Returns the number of zeros in the binary representation of `self`.
+            pub const fn count_zeros(self) -> u32 {
+                self.get().count_zeros()
+            }
+
+            #[cfg(feature = "checked-ops")]
+            checked_ops!($type);
+
         }
 
         impl PartialEq for $type {
@@ -135,6 +465,39 @@ macro_rules! non_max_impl {
             }
         }
 
+        impl_binop!(impl Add, add, $type);
+        impl_binop!(impl Sub, sub, $type);
+        impl_binop!(impl Mul, mul, $type);
+        impl_binop!(impl Div, div, $type);
+
+        impl_binop!(impl BitAnd, bitand, $type);
+        impl_binop!(impl BitOr, bitor, $type);
+        impl_binop!(impl BitXor, bitxor, $type);
+
+        impl_assign_op!(impl AddAssign, add_assign, Add, add, $type);
+        impl_assign_op!(impl SubAssign, sub_assign, Sub, sub, $type);
+        impl_assign_op!(impl MulAssign, mul_assign, Mul, mul, $type);
+        impl_assign_op!(impl DivAssign, div_assign, Div, div, $type);
+
+
+        impl_assign_op!(impl BitAndAssign, bitand_assign, BitAnd, bitand, $type);
+        impl_assign_op!(impl BitOrAssign, bitor_assign, BitOr, bitor, $type);
+        impl_assign_op!(impl BitXorAssign, bitxor_assign, BitXor, bitxor, $type);
+
+        impl TryFrom<$primitive> for $type {
+            type Error = PrimitiveIsMaxError<$primitive>;
+
+            fn try_from(value: $primitive) -> Result<Self, Self::Error> {
+                Self::new(value).ok_or(PrimitiveIsMaxError(Self::INVALID_UNDERLYING))
+            }
+        }
+
+        impl Into<$primitive> for $type {
+            fn into(self) -> $primitive {
+                self.get()
+            }
+        }
+
         #[cfg(test)]
         mod $test_name {
             use super::*;
@@ -147,6 +510,20 @@ macro_rules! non_max_impl {
         }
     };
 }
+
+/// Error type returned by [TryFrom] from primitive integer into nonmax versions
+///
+/// See [U7], [U15], [U31], [U63]
+#[derive(Debug, Clone, Copy)]
+pub struct PrimitiveIsMaxError<T>(pub T);
+
+impl<T: core::fmt::Display> core::fmt::Display for PrimitiveIsMaxError<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.write_fmt(format_args!("PrimitiveIsMaxError({})", self.0))
+    }
+}
+
+impl<T: core::fmt::Display + core::fmt::Debug> core::error::Error for PrimitiveIsMaxError<T> {}
 
 non_max_impl!(U7, u8, u7_test);
 non_max_impl!(U15, u16, u15_test);
